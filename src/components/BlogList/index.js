48c32aef4573600f65ee362819d1c09294c95cff
Prashanthjr431@gmail.com <Prashanthjr431@gmail.com>
2023-06-19 07:53:10 +0000

    blogList

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+import BlogItem from '../BlogItem'
+
+const BlogList = props => {
+  const {blogsList} = props
+  return (
+    <ul className="blogList">
+      {blogsList.map(each => (
+        <BlogItem blogitemDetails={each} key={each.id} />
+      ))}
+    </ul>
+  )
+}
+
+export default BlogList
---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="app-container">
+    <div className="user-info-container">
+      <img
+        alt="profile"
+        src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+        className="image-user"
+      />
+      <h1 className="name">Wade Warren</h1>
+      <p className="role">software developer at UK</p>
+    </div>
+  </div>
+)
+
+export default UserInfo
